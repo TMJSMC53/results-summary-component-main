42707d04b925c1e6e538b707359e3152323b3899
Triola Jackson <triolajackson@gmail.com>
2023-04-27 19:20:01 -0600

    Added all of the mobile styles

--- summary-results/src/components/Summary.jsx
+++ summary-results/src/components/Summary.jsx
@@ -2,8 +2,43 @@ import React from "react";
 
 const Summary = () => {
   return (
-    <div>
-      Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 / 100
+    <div className="summarySection">
+      <h2>Summary </h2>
+      <div className="summaryContainer">
+        <div className="box reaction">
+          <img src="../assets/images/icon-reaction.svg" alt="" />
+          <p>Reaction</p>
+
+          <span className="resultsBox">
+            80
+            <span className="results">/100</span>
+          </span>
+        </div>
+        <div className="box memory">
+          <img src="../assets/images/icon-memory.svg" alt="" />
+          <p>Memory</p>
+          <span className="resultsBox">
+            92 <span className="results"> /100</span>
+          </span>
+        </div>
+        <div className="box verbal">
+          <img src="../assets/images/icon-verbal.svg" alt="" />
+          <p>Verbal</p>
+          <span className="resultsBox">
+            61 <span className="results">/100</span>
+          </span>
+        </div>
+        <div className="box visual">
+          <img src="../assets/images/icon-visual.svg" alt="" />
+
+          <p>Visual</p>
+          <span className="resultsBox">
+            72 <span className="results">/100</span>
+          </span>
+        </div>
+      </div>
+
+      <button className="btn">Continue</button>
     </div>
   );
 };
